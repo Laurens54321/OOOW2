fa3964a97ced367d0468db2dbd978e6c77df810c
Laurens54321 <laurens.hons@gmail.com>
2020-10-16 13:55:09 +0200

    Initial Commit

---
+++ src/launcher/Main.java
@@ -0,0 +1,12 @@
+package launcher;
+
+import ui.view.EncryptorUI;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        EncryptorUI encryptorUI = new EncryptorUI();
+        encryptorUI.openEncryptor();
+    }
+}
